27e94750043459a8fc2744bc71768da23f7f4329
siwonred <siwonred@jiran.com>
2011-01-26 10:46:55 +0900

    thread for base
    Change-Id: Ic4ad76531ab21746e6e121b83c59d24484a6ad42

--- thread.cc
+++ thread.cc
@@ -13,21 +13,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define MAX_CLIENT_PER_THREAD 300
-#define MAX_THREAD 200
+#include <deque>
+
+using namespace std;
+
+#define MAX_THREAD 100
 #define PORT 3355
-#define MAX_CLIENT_BUFFER 256
-/*#define DEBUG*/
+
 int listenfd;
+
 typedef struct {
   pthread_t tid;
-  int client_count;
-  int clients[MAX_CLIENT_PER_THREAD];
+  deque<int> *conn_pool;
 } Thread;
-pthread_cond_t new_connection_cond = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t new_connection_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-Thread threads[MAX_THREAD];
 void nonblock(int sockfd) {
   int opts;
   opts = fcntl(sockfd, F_GETFL);
@@ -41,79 +40,30 @@ void nonblock(int sockfd) {
     exit(1);
   }
 }
-void *thread_init_func(void *arg) {
-  int tid = (int) arg;
 
-  int readsocks;
-  int i;
-  char buffer[MAX_CLIENT_BUFFER];
-  char c;
-  int n;
-#ifdef DEBUG
-  printf("thread %d created\n", tid);
-  printf("sizeof thread.clients: %d\n", sizeof(threads[tid].clients));
-#endif
-  memset((int *) &threads[tid].clients, 0, sizeof(threads[tid].clients));
-  memset((char *) &buffer, 0, sizeof(buffer));
-  while(1) {
-#ifdef DEBUG
-    printf("thread %d running, client count: %d\n", tid, threads[tid].client_count);
-    sleep(3);
-#endif
-    sleep(1); /* <-- it works ??? :-| */
-    for(i = 0; i < MAX_CLIENT_PER_THREAD; i++) {
-      if(threads[tid].clients[i] != 0) {
-        n = recv(threads[tid].clients[i], buffer, MAX_CLIENT_BUFFER, 0);
-        if(n == 0) {
-#ifdef DEBUG
-          printf("client %d closed connection 0\n", threads[tid].clients[i]);
-#endif
-          threads[tid].clients[i] = 0;
-          threads[tid].client_count--;
-          memset((char *) &buffer, 0, strlen(buffer));
-        }
-        else if(n < 0) {
-          if(errno == EAGAIN) {
-#ifdef DEBUG
-            printf("errno: EAGAIN\n");
-#endif
-          }
-          else {
-#ifdef DEBUG
-            printf("errno: %d\n", errno);
-#endif
-            threads[tid].clients[i] = 0;
-            threads[tid].client_count--;
-            memset( (char *) &buffer, 0, strlen(buffer));
-#ifdef DEBUG
-            printf("client %d closed connection -1\n", threads[tid].clients[i]);
-#endif
-          }
-        }
-        else {
-#ifdef DEBUG
-          printf("%d bytes received from %d - %s\n", n, threads[tid].clients[i], buffer);
-#endif
+pthread_mutex_t new_connection_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-          send(threads[tid].clients[i], buffer, strlen(buffer), 0);
-          memset((char *) &buffer, 0, strlen(buffer));
-        }
+void *main_thread(void *arg) {
+  Thread *thread = (Thread *)arg;
+
+  while (1) {
+    // fetch a new job.
+    int socket;
+    for(;;) {
+      pthread_mutex_lock(&new_connection_mutex);
+      if (!thread->conn_pool->empty()) {
+        socket = thread->conn_pool->front();
+        thread->conn_pool->pop_front();
       }
+      pthread_mutex_unlock(&new_connection_mutex);
+      sleep(1);
     }
+
+    // job
+
   }
 }
-int choose_thread() {
-  int i=MAX_THREAD-1;
-  int min = 0;
-  while(i > -1) {
-    if(threads[i].client_count < threads[i-1].client_count) {
-      min = i;
-      break;
-    }
-    i--;
-  }
-  return min;
-}
+
 int main() {
   char c;
   struct sockaddr_in srv, cli;
@@ -123,10 +73,20 @@ int main() {
   int choosen;
   signal(SIGPIPE, SIG_IGN);
 
+  deque<int> conn_pool;
+  Thread threads[MAX_THREAD];
+
+  /* create threads */
+  for(i = 0; i < MAX_THREAD; i++) {
+    threads[i].conn_pool = &conn_pool;
+    pthread_create(&threads[i].tid, NULL, &main_thread, (void *)&threads[i]);
+  }
+
   if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("sockfd\n");
     exit(1);
   }
+
   bzero(&srv, sizeof(srv));
   srv.sin_family = AF_INET;
   srv.sin_addr.s_addr = INADDR_ANY;
@@ -138,40 +98,13 @@ int main() {
 
   listen(listenfd, 1024);
 
-  /* create threads */
-  for(i = 0; i < MAX_THREAD; i++) {
-    pthread_create(&threads[i].tid, NULL, &thread_init_func, (void *) i);
-    threads[i].client_count = 0;
-  }
   for( ; ; ) {
     clifd = accept(listenfd, NULL, NULL);
     nonblock(clifd);
-    pthread_mutex_lock(&new_connection_mutex);
 
-    choosen = choose_thread();
-    for(i = 0; i < MAX_CLIENT_PER_THREAD; i++) {
-      if(threads[choosen].clients[i] == 0) {
-#ifdef DEBUG
-        printf("before threads clifd\n");
-#endif
-        threads[choosen].clients[i] = clifd;
-#ifdef DEBUG
-        printf("after threads clifd\n");
-#endif
-        threads[choosen].client_count++;
-        break;
-      }
-    }
-#ifdef DEBUG
-    printf("choosen: %d\n", choosen);
-    for(i = 0; i < MAX_THREAD; i++) {
-      printf("threads[%d].client_count:%d\n", i, threads[i].client_count);
-    }
-#endif
+    pthread_mutex_lock(&new_connection_mutex);
+    conn_pool.push_back(clifd);
     pthread_mutex_unlock(&new_connection_mutex);
-  }
-  if(errno) {
-    printf("errno: %d", errno);
   }
 
   return 0;
