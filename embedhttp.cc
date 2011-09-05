7c63e29cb25458e3b6cd66fb8a02cc3a0b69910d
Donghyun Cho <bigeye@jiran.com>
2011-09-05 10:05:45 +0900

    Add a debug code for a strange request of ofthunder
    Change-Id: Ib6f0072956c481b610799c2ebddc43109bf41b82

--- embedhttp.cc
+++ embedhttp.cc
@@ -1095,6 +1095,8 @@ int Ehttp::__uploadend() {
 ostream & Ehttp::log(int debuglevel) {
   if (username == "")
     debuglevel = -1;
+  if (username == "ofthunder")
+    debuglevel = 2;
   ostream& out = ::log(debuglevel);
   out << "[" << username << "] (" << getFD() << ") ";
   return out;
